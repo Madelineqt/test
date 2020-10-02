e957998be1def0b78c5358de1af639cb616da8d2
sistemas-y-redes <sistemas-y-redes>
2020-10-02 10:04:33 +0200

    changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 15000
 var mysql = require('mysql');
 app.get('/test', (req, res) => {
   var connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : 'localhost:3306',
     user     : 'db_test',
     password : '*S4d30vs',
     database : 'admin_test'
@@ -12,7 +12,7 @@ app.get('/test', (req, res) => {
    
   connection.connect();
    
-  connection.query('SELECT * FROM "test"', function (error, results, fields) {
+  connection.query("SELECT * FROM 'test'", function (error, results, fields) {
     if (error) throw error;
     res.send('The solution is: ', results[0].solution);
   });
