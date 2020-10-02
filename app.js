e2cfb580440eeec69a01e6a9b1aa51e43cdc5013
sistemas-y-redes <sistemas-y-redes>
2020-10-02 10:01:12 +0200

    test

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 15000
-
+var mysql = require('mysql');
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'db_test',
+    password : '*S4d30vs',
+    database : 'admin_test'
+  });
+   
+  connection.connect();
+   
+  connection.query('SELECT * FROM "test"', function (error, results, fields) {
+    if (error) throw error;
+    res.send('The solution is: ', results[0].solution);
+  });
+   
+  connection.end();
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
